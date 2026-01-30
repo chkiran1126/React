0a32f6774fabfa8258914214e344f7832e2105ac
chkiran1126 <chkiran1126@gmail.com>
2026-01-31 00:33:55 +0530

    using Tailwind CSS and learn about props in React.

--- my-react-app/src/counter.jsx
+++ my-react-app/src/counter.jsx
@@ -3,11 +3,22 @@ import { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
 
+  const increase = () => {
+    if (count < 20) {
+      setCount(count + 1);
+    }
+  };
+  const decrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <>
       <h1>Counter:{count}</h1>
-      <Buttons text="+" action={() => setCount(count + 1)} />
-      <Buttons text="-" action={() => setCount(count - 1)} />
+      <Buttons text="+" action={increase} />
+      <Buttons text="-" action={decrease} />
       <Buttons text="Reset" action={() => setCount(0)} />
     </>
   );
